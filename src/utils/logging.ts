ef1999a8a4d99627cca2f27272a663b749ece9ef
0tickpulse <0tickpulse@gmail.com>
2023-08-06 21:56:58 +0800

    use server.connection.console.log

--- src/utils/logging.ts
+++ src/utils/logging.ts
@@ -2,6 +2,7 @@ import { appendFile } from "fs";
 import { Color, codes } from "tick-ts-utils";
 import { format } from "util";
 import { TextDocumentIdentifier } from "vscode-languageserver";
+import { server } from "../index.js";
 
 let prevTime = Date.now();
 
@@ -9,21 +10,26 @@ function fmtLog(str: string, color: Color) {
     const now = Date.now();
     const diff = now - prevTime;
     prevTime = now;
-    return `[${ new Date().toLocaleTimeString() }] ` + color.toAnsiColorCode() + str + codes.reset + ` (${ diff }ms)`;
+    const res = `[${new Date().toLocaleTimeString()}] ` + color.toAnsiColorCode() + str + codes.reset + ` (${diff}ms)`;
+    return res;
+}
+
+function log(str: string) {
+    server.connection?.console.log(str);
 }
 
 export function info<T extends unknown[]>(prefix = "Mythic Language Server", ...args: T): T {
-    console.log(fmtLog(format(`[${prefix}: INFO]`, ...args), Color.parseHex("#55FF55").get()));
+    log(fmtLog(format(`[${prefix}: INFO]`, ...args), Color.parseHex("#55FF55").get()));
     return args;
 }
 
 export function warn<T extends unknown[]>(prefix = "Mythic Language Server", ...args: T): T {
-    console.log(fmtLog(format(`[${prefix}: WARN]`, ...args), Color.parseHex("#FFFF55").get()));
+    log(fmtLog(format(`[${prefix}: WARN]`, ...args), Color.parseHex("#FFFF55").get()));
     return args;
 }
 
 export function dbg<T extends unknown[]>(prefix = "Mythic Language Server", ...args: T): T {
-    console.log(fmtLog(format(`[${prefix}: DEBUG]`, ...args), Color.parseHex("#55FFFF").get()));
+    log(fmtLog(format(`[${prefix}: DEBUG]`, ...args), Color.parseHex("#55FFFF").get()));
     return args;
 }
 
